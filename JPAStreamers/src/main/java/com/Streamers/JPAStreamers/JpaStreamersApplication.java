2c673e22a6442f0fc9a484ebf12e5458e721dd98
rithishmamidala <20r21a1287@mlrinstitutions.ac.in>
2024-07-26 18:46:03 +0530

    c1

--- JPAStreamers/src/main/java/com/Streamers/JPAStreamers/JpaStreamersApplication.java
+++ JPAStreamers/src/main/java/com/Streamers/JPAStreamers/JpaStreamersApplication.java
@@ -2,8 +2,7 @@ package com.Streamers.JPAStreamers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.Streamers.JPAStreamers;", "com.speedment.jpastreamer"})
 public class JpaStreamersApplication {
 
 	public static void main(String[] args) {
--- JPAStreamers/src/main/java/com/Streamers/JPAStreamers/controller/StreanControl.java
+++ JPAStreamers/src/main/java/com/Streamers/JPAStreamers/controller/StreanControl.java
@@ -2,10 +2,13 @@ package com.Streamers.JPAStreamers.controller;
 
 import com.Streamers.JPAStreamers.model.Employee;
 import com.Streamers.JPAStreamers.repository.Repo;
+import com.speedment.jpastreamer.application.JPAStreamer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1")
@@ -13,6 +16,18 @@ public class StreanControl {
 
     @Autowired
     private Repo repo;
+
+    private JPAStreamer jpaStreamer;
+
+    @Autowired
+    public StreanControl(final JPAStreamer jpaStreamer) {
+        this.jpaStreamer = jpaStreamer;
+    }
+    @GetMapping("/groupById")
+    public Map<String, List<Employee>> groupbyEmployeeByCity() {
+        return jpaStreamer.stream(Employee.class)
+                .collect(Collectors.groupingBy(Employee::getCity));
+    }
     @PostMapping("/save")
     public Employee saveEmployee(@RequestBody Employee employee) {
         return repo.save(employee);
